a620890f541abb6f1f96911b8b10186fb20b2a24
Jennifer66-L <jl12731@nyu.edu>
2024-12-10 01:26:01 -0500

    final web

---
+++ Midterm /j'vrnvfwnv/data.js
@@ -0,0 +1,50 @@
+const cardInfo = [
+    {
+        bgColor: '255,188,75',
+        imgUrl: './asset/img/8.jpg',
+        tips: 'I LOVE',
+        contnetText: 'My Dog Dollar!!!!!'
+    },
+    {
+        bgColor: '255,188,75',
+        imgUrl: './asset/img/7.jpg',
+        tips: 'I LOVE',
+        contnetText: 'My Dog Dollar'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: './asset/img/6.jpg',
+        tips: 'I LOVE',
+        contnetText: 'Traveling'
+    },
+    {
+        bgColor: '255,188,75',
+        imgUrl: './asset/img/5.jpg',
+        tips: 'I LOVE',
+        contnetText: 'Cooking'
+    },
+    {
+        bgColor: '164,201,216',
+        imgUrl: './asset/img/4.jpg',
+        tips: 'I LOVE',
+        contnetText: 'Snowboarding'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: './asset/img/3.jpg',
+        tips: 'I LOVE',
+        contnetText: 'Listening Jazz & Afro'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: './asset/img/2.jpg',
+        tips: 'I LOVE',
+        contnetText: 'Doing Nail Arts'
+    },
+    {
+        bgColor: '255,208,213',
+        imgUrl: './asset/img/1.jpg',
+        tips: 'WHO AM I',
+        contnetText: 'I am Jennifer (Jia) Liu, from Mainland China, borned and raised in Qingdao, a coastal city where made TSINGTAO BEER'
+    },
+]
